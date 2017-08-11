b03916ccbdfca9df613bbec63c2939eb12de93ab
Brian Wilgus <contact@brianwilgus.com>
2017-08-10 20:24:16 -0400

    wip material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { 
-  MdButtonModule, 
-  MdCheckboxModule, 
-  MdIconModule,
-  MdToolbarModule 
-} from '@angular/material';
+import { MaterialModule } from '@angular/material';
 
 import { WelcomeComponent } from './welcome/welcome.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
@@ -27,12 +24,11 @@ import { Routing } from './app.routing';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule,
+    FormsModule,
+    HttpModule,
     Routing,
-    MdButtonModule,
-    MdCheckboxModule,
-    MdIconModule,
-    MdToolbarModule
+    MaterialModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
